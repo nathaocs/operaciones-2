db68818610a781e34da8b1e5e104618faa9e7a85
lauv538 <ls.villamilp@gmail.com>
2025-03-25 13:01:42 -0500

    Update index.py

--- index.py
+++ index.py
@@ -1,7 +1,1 @@
-import math
 
-c1 = float(input("Ingrese el valor del cateto1: "))
-c2 = float(input("Ingrese el valor del cateto2: "))
-
-h = float(math.sqrt(c1**2 + c2**2))
-print("La hipotenusa es: {h}")
