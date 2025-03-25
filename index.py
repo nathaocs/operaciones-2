6417b211ddb6ebc58313681a589337859ac741b7
JulianaRamon <andreaplanta1002@gmail.com>
2025-03-25 13:01:58 -0500

    Update index.py

--- index.py
+++ index.py
@@ -1,7 +1,1 @@
-import math
-numero = float(input("Introduce un número para calcular su logaritmo natural: "))
-if numero > 0:
-    logaritmo = math.log(numero)
-    print("El logaritmo natural es:" + logaritmo)
-else:
-    print("El número debe ser mayor que cero para calcular el logaritmo natural.")
+
