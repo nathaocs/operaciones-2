383b312ecf8f37c8c5f5d71892eb6b22e89c34f1
NataliaCS <nataliacadena2@gmail.com>
2025-03-25 18:29:57 -0500

    Agregada función division

---
+++ OPERACIONES/funciondivision.py
@@ -0,0 +1,8 @@
+def dividir(a, b):
+    resultado = a / b
+    print("Resultado:", resultado) 
+
+num1 = float(input("Ingresa el primer número: "))
+num2 = float(input("Ingresa el segundo número: "))
+
+dividir(num1, num2)
