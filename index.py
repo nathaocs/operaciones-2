2296ddb39c1c0c8642b843510ef600629bcd7d39
lauv538 <ls.villamilp@gmail.com>
2025-03-25 12:58:33 -0500

    pitagoras.py

--- index.py
+++ index.py
@@ -1,1 +1,7 @@
-## Aca llama a la funcion
+import math
+
+c1 = float(input("Ingrese el valor del cateto1: "))
+c2 = float(input("Ingrese el valor del cateto2: "))
+
+h = float(math.sqrt(c1**2 + c2**2))
+print("La hipotenusa es: {h}")
