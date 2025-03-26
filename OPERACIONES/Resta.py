472f02b129283d366800c4eebb479575f9bb05f3
Juan Beltrán <2009jdbg@gmail.com>
2025-03-26 08:18:11 -0500

    Resta

---
+++ OPERACIONES/Resta.py
@@ -0,0 +1,7 @@
+def resta():
+    res=a-b
+    print (r'la resta de los numeros {a} y {b} es {res}')
+    return
+a=(int(input('ingrese el valor de a=')))   
+b=(int(input('ingrese el valor de b=')))
+resta()
