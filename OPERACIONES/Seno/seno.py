12d8e12e5ab258ad74a7d697e5bda62e9b76c7e6
EmilyManu <emilywalterosnieto@gmail.com>
2025-03-26 08:40:37 -0500

    Update seno.py

--- OPERACIONES/Seno/seno.py
+++ OPERACIONES/Seno/seno.py
@@ -1,5 +1,7 @@
 import math
+grados = float(input("ingrese el angulo en grados"))
 def seno(angulo):
     angulo=math.radians(angulo)
     seno=math.sin(angulo)
     return seno 
+print(seno(grados))
