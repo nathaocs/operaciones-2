e6a1bbd0348c4ad4cf288dd49919288731251bc9
JuanK3105 <lozanojuancamilo37@gmail.com>
2025-04-10 20:47:50 -0500

    agrego sintaxis de función

--- OPERACIONES/csc-1.py
+++ OPERACIONES/csc-1.py
@@ -1,10 +1,12 @@
-import math 
+import math
 
-x = float(input("Entrada: "))
+def funcioncosecanteala1(x):
+    if abs(x) < 1:
+        return "Cosecante a la -1 solo está definida para |x| ≥ 1"
+    else:
+        angulo = math.asin(1 / x)
+        return f"csc⁻¹({x}) = {angulo} radianes"
 
-if abs(x) < 1:
-    print("Cosecante a la -1 solo está definida para |x| ≥ 1")
-else:
-    angulo = math.asin(1 / x)
-    print(f"csc⁻¹({x}) = {angulo} radianes")
-    
+entrada = float(input("Entrada: "))
+resultado = funcioncosecanteala1(entrada)
+print(resultado)
